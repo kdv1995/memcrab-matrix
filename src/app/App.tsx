497ad49de3897f15efa7d56f14999df8a470f29f
kdv1995 <tribeofdanel@gmail.com>
2023-03-07 07:59:31 +0200

    Fixing unnecassary comments

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,12 @@
 import { MatrixProvider } from "context/matrixContext";
 import { FC } from "react";
 import { ReactRouter } from "router";
-import { StoreProvider } from "store";
 export const App: FC = () => {
   return (
     <>
-      <StoreProvider>
-        <MatrixProvider>
-          <ReactRouter />
-        </MatrixProvider>
-      </StoreProvider>
+      <MatrixProvider>
+        <ReactRouter />
+      </MatrixProvider>
     </>
   );
 };
--- src/context/appContext.tsx
+++ src/context/appContext.tsx
@@ -1,17 +1,1 @@
-type Action =
-  | { type: "SET_MATRIX"; payload: [] }
-  | { type: "SET_INCREMENT"; payload: number }
-  | { type: "SET_NUMBER_TO_CUT"; payload: number }
-  | { type: "SET_CLOSEST_VALUES"; payload: number }
-  | { type: "SET_CLEAR_VALUES" }
-  | { type: "SET_NEW_ROW_DATA"; payload: {} }
-  | { type: "SET_ROW_PERCENTAGE" }
-  | { type: "SET_CLEAR_DEPOSIT" }
-  | { type: "SET_NEW_ROW" }
-  | { type: "SET_DELETE_ROW"; payload: string };
-
-type State = {
-  matrix: [];
-  newRowData: {};
-  numberToShow: number;
-};
+export {};
